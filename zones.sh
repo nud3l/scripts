a9ac82eeff0d759b79e2588185420d8b935e46b0
Dominik Harz <dominik.harz@gmail.com>
2019-09-12 16:09:39 +0900

    include help function

--- zones.sh
+++ zones.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-DATE=$1
+OPTIND=1 
+DATE=""
+
+USAGE="Display a time in various timezones. Usage:
+
+	-h	show this help
+	-d	specify a date, time, and timezone
+
+Examples:
+zones -d 'TZ=\"Asia/Jerusalem\" 09:00 next Fri'
+zones -d '09:00 next Mon'
+
+Use tzselect to find your time zone."
+
+
+while getopts "h?d:" option; do
+	case "${option}" in
+	h|\?) echo "$USAGE"
+	      exit
+	      ;;
+	d) DATE=$OPTARG
+	   ;;
+	esac
+done
+
+shift $((OPTIND - 1))
 
 echo "Timezones"
 TZ=Asia/Tokyo date --date="$DATE"
